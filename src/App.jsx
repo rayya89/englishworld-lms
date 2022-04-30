1f4fd067752217b882814e12d810559759fc84af
Rayya <rayyatar@gmail.com>
2022-04-30 20:07:27 +0200

    add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+//NPM packages
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//Project files
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <div>
+      <p>
+        Welcome to English World! English World is one of the leading
+        educational platform that holds course content, materials and
+        administration in one user-friendly online system. It allows teachers to
+        easily manage courses and track their students, highlighting areas of
+        strength and weakness for ongoing performance improvement.
+      </p>
+      <p>Sign up for free and start Learning now!</p>
+      <Link to="/sign-up">Sign up</Link>
+    </div>
+  );
+}
