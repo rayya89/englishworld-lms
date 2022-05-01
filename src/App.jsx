c9d95fd43f68b19922448d83beb1bfb79682ff9e
Rayya <rayyatar@gmail.com>
2022-05-01 15:53:25 +0200

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,10 @@ import StudentsRoutes from "./routes/StudentsRoutes";
 export default function App() {
   //Local state
   const [uid, setUid] = useState(null);
-  console.log("App.jsx", uid);
 
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="sign-up" element={<SignUp />} />
-          <Route path="dashboard" element={<Dashboard />} />
-        </Routes> */}
         {uid && <StudentsRoutes uidState={[uid, setUid]} />}
         {!uid && <UnloggedRoutes uidState={[uid, setUid]} />}
       </BrowserRouter>
